11a894005341ac1269b0167bfd529c973e2ffaa2
lilmrsgowin <logowin@microsoft.com>
2017-02-08 10:04:40 -0600

    Project Init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var myname = "Lori Gowin";
+            Console.WriteLine("Hello " + myname);
+            Console.ReadKey();
+        }
+    }
+}
